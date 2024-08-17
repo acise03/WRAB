2cb508be79e1b63157172b250d5bf5132b8f7f5c
Grace <33011813+gpu314@users.noreply.github.com>
2024-08-17 14:52:56 -0400

    Route to roads

--- addRoute.js
+++ addRoute.js
@@ -1,4 +1,5 @@
 let mode = null;
+const API_KEY = 'iJhhowAaIrAcbRVdJLMHfrj5e4v2VekB';
 
 function toggleMode(button) {
     mode = button;
@@ -8,57 +9,36 @@ function toggleMode(button) {
 map.on('click', (event) => {
     if (mode) {
         const coords = [event.lngLat.lng, event.lngLat.lat];
-        document.getElementById(mode+'Coord').value = coords.join(', ');
+        document.getElementById(mode + 'Coord').value = coords.join(', ');
         mode = null;
     }
 });
 
-document.getElementById('startCoordButton').addEventListener('click', (event) => {event.preventDefault(); toggleMode('start')});
-document.getElementById('endCoordButton').addEventListener('click', (event) => {event.preventDefault(); toggleMode('end')});
+document.getElementById('startCoordButton').addEventListener('click', (event) => { event.preventDefault(); toggleMode('start') });
+document.getElementById('endCoordButton').addEventListener('click', (event) => { event.preventDefault(); toggleMode('end') });
 
-function addRoute() {
+async function addRoute() {
     const routeName = document.getElementById('routeName').value;
     const startCoord = document.getElementById('startCoord').value.split(',').map(Number);
     const endCoord = document.getElementById('endCoord').value.split(',').map(Number);
-    
-    const newRouteGeoJson = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "LineString",
-                    "coordinates": [
-                        startCoord,
-                        endCoord
-                    ]
-                },
-                "properties": {
-                    "name": routeName
-                }
-            }
-        ]
-    };
-    
-    if (map.getSource('userRoute')) {
-        map.getSource('userRoute').setData(newRouteGeoJson);
-    }
-    else {
-        map.addLayer({
-            id: 'userRoute',
-            type: 'line',
-            source: {
-                type: 'geojson',
-                data: newRouteGeoJson
-            },
-            layout: {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            paint: {
-                'line-color': '#0000FF',
-                'line-width': 5
-            }
-        });
-    }
+   
+
+    var response = await tt.services.calculateRoute({
+        key: API_KEY,
+        locations: `${startCoord.join(',')}:${endCoord.join(',')}`,
+    });
+
+    var newRouteGeoJson = response.toGeoJson()
+    map.addLayer({
+        id: "route",
+        type: "line",
+        source: {
+            type: "geojson",
+            data: newRouteGeoJson,
+        },
+        paint: {
+            "line-color": "#0000FF",
+            "line-width": 5,
+        },
+    })
 }
